3a888cbd6174f77c250539f4358dd4a72028269f
Pranshu Diwakar <pranshupranshu92153@gmail.com>
2026-03-28 09:36:06 +0530

    Fix Doctors route as component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Hospitals from "./pages/Hospitals"; // ✅ Import Hospitals
+import Hospitals from "./pages/Hospitals";
+import Doctors from "./pages/Doctors"; // ✅ Import Hospitals
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/hospitals" element={<Hospitals />} /> {/* ✅ Add this */}
-        <Route path="/doctors" element={<div>Doctors Page</div>} />
+        <Route path="/hospitals" element={<Hospitals />} />
+        <Route path="/doctors" element={<Doctors />} />
         <Route path="/ambulance" element={<div>Ambulance Page</div>} />
       </Routes>
     </BrowserRouter>
