d55d15d621e5ba0bf927703748938080cc4b9bdb
Pranshu Diwakar <pranshupranshu92153@gmail.com>
2026-07-02 13:02:11 +0530

    final backend fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://emergency-backend-5.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
